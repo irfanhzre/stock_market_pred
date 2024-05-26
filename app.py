12234c7e7d5777cfdc5c55bf776cb885a0c0b98e
irfanhzre <92743466+irfanhzre@users.noreply.github.com>
2024-05-27 04:36:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import streamlit as st
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-import pandas as pd
+
 
 
 # Step 1: Load your dataset
